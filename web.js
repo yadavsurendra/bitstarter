1e411bd6e090605f11bde1b5628f9d7d36a36a30
Ubuntu <ubuntu@ip-172-31-2-1.us-west-2.compute.internal>
2013-07-12 06:04:36 +0000

    Changed msg

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 var msg = fs.readFile('index.html', function (err, data)
 
 app.get('/', function(request, response) {
-  response.send(msg);
+  response.send('Hello World from index.html');
 });
 
 var port = process.env.PORT || 5000;
